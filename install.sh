a68b6b1cefc4272888def6d55ac6a163803b4b66
ELERMAND <57151178+ELERMAND@users.noreply.github.com>
2020-02-05 23:06:38 +0200

    ghgfh
    ghg

--- install.sh
+++ install.sh
@@ -1,29 +1,3 @@
-#!/bin/bash
-clear
-from __future__ import unicode_literals
-import os
-import sys
-import time
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from halo import Halo
-
-spinner = Halo(text='Such Spins', spinner='dots')
-
-try:
-    spinner.start()
-    time.sleep(2)
-    spinner.text = 'Much Colors'
-    spinner.color = 'magenta'
-    time.sleep(2)
-    spinner.text = 'Very emojis'
-    spinner.spinner = 'hearts'
-    time.sleep(2)
-    spinner.stop_and_persist(symbol='🦄'.encode('utf-8'), text='Wow!')
-except (KeyboardInterrupt, SystemExit):
-    spinner.stop()
-    
 echo "===================="
 echo "|    How are you?  |"
 echo "|======Select======|"
